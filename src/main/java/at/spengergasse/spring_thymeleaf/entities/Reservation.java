00429b78f3af803460767c5606f3a8bb786b38e4
rebecca <pop230399@spengergasse.at>
2026-04-24 22:08:43 +0200

    aufgabe10_radiologie_popa_version2 fertig

--- src/main/java/at/spengergasse/spring_thymeleaf/entities/Reservation.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/entities/Reservation.java
@@ -1,19 +1,68 @@
 package at.spengergasse.spring_thymeleaf.entities;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "reservation")        //wie db heißen
 public class Reservation
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
 
+    @ManyToOne
+    @JoinColumn(name = "device_id")
+    private Device device;
+
+    public Reservation()
+    {
+
+    }
+
+    public Reservation(Patient patient, Device device, LocalDate date, String timeFrom, String timeTo, String bodyRegion, String comment) {
+        this.patient = patient;
+        this.device = device;
+        this.date = date;
+        this.timeFrom = timeFrom;
+        this.timeTo = timeTo;
+        this.bodyRegion = bodyRegion;
+        this.comment = comment;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public Device getDevice()
+    {
+        return device;
+    }
+
+    public void setDevice(Device device)
+    {
+        this.device = device;
+    }
+
+
+    public void setPatient(Patient patient)
+    {
+        this.patient = patient;
+    }
     private LocalDate date;
     private String timeFrom;
     private String timeTo;
@@ -30,7 +79,5 @@ public class Reservation
     public void setBodyRegion(String bodyRegion) { this.bodyRegion = bodyRegion; }
     public String getComment() { return comment; }
     public void setComment(String comment) { this.comment = comment; }
-
-
 
 }
